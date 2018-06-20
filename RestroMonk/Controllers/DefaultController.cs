1c6992d7d1b396866464303c5180967453493aca
Neha Ambasta <nehaambasta08@gmail.com>
2018-06-20 14:18:20 +0530

    Initial commit

---
+++ RestroMonk/Controllers/DefaultController.cs
@@ -0,0 +1,30 @@
+﻿using RestroMonk.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace RestroMonk.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            return new List<Account>
+            {
+                new Account{ AccountNumber="0001", BankName="Dummy Bank", CustomerAddress= new Address
+                {
+                     Address1="address1", Address2="address2", Country="US", PostalCode="20877", State="MD"
+                }, CustomerId="Customer001", CustomerName="John Sherman"
+                },  new Account{ AccountNumber="0002", BankName="Dummy Bank", CustomerAddress= new Address
+                {
+                     Address1="address3", Address2="address4", Country="US", PostalCode="20877", State="MD"
+                }, CustomerId="Customer002", CustomerName="Marie Curie"
+                }
+            };
+        }
+    }
+}
---
+++ RestroMonk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RestroMonk.WebApiApplication" Language="C#" %>
